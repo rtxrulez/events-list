3a3d30f6d5ee95fadd3a23fa4092e69fae22b5b8
Ramil <vafarulez@ya.ru>
2019-09-21 22:58:22 +0400

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Мероприятия</h1>
       </header>
+      <section className="list">
+        <div className="list__header">
+          <div className="list__header-control">
+            <button className="btn btn-transparent">+</button>
+            <button className="btn btn-transparent">+</button>
+          </div>
+          <div className="list__search">
+              <input type="text" className="form-control list__search-input" placeholder="Поиск" />
+          </div>
+        </div>
+        <div className="list__content">
+          <div className="list__item list__item--head">
+            <div className="list__item-checkbox"></div>
+            <div className="list__item-name">Название</div>
+            <div className="list__item-date">Дата</div>
+            <div className="list__item-location">Место проведения</div>
+          </div>
+          <div className="list__scroll">
+            <div className="list__item">
+              <div className="list__item-checkbox">
+                <label htmlFor="" className="checkbox">
+                  <input type="checkbox" />
+                  <div className="cr"></div>
+                </label>
+              </div>
+              <div className="list__item-name">Мероприятие 1</div>
+              <div className="list__item-date">20.04.2018</div>
+              <div className="list__item-location">Саратов</div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
