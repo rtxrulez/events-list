bb4b60fe8735f1493f2252e80413050d568b7f23
Ramil <vafarulez@ya.ru>
2019-09-22 13:06:06 +0400

    Updates

---
+++ precache-manifest.1706ea0c9cae9bc7bd6a9f7df00d6e3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be622b6ed2f77d21f60499e33735ef72",
+    "url": "/events-list/index.html"
+  },
+  {
+    "revision": "2eea8615b257dbc84761",
+    "url": "/events-list/static/css/main.117125f6.chunk.css"
+  },
+  {
+    "revision": "a4348f8b8cda0d1bb8f9",
+    "url": "/events-list/static/js/2.4bd4356e.chunk.js"
+  },
+  {
+    "revision": "2eea8615b257dbc84761",
+    "url": "/events-list/static/js/main.accef571.chunk.js"
+  },
+  {
+    "revision": "511d33463a9173dd7170",
+    "url": "/events-list/static/js/runtime~main.fd04c372.js"
+  }
+]);
