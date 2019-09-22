6105b0afffb21e5bcc88972508436ca2fe933de4
Ramil <vafarulez@ya.ru>
2019-09-22 22:27:54 +0400

    интерфейс

--- src/App.js
+++ src/App.js
@@ -10,16 +10,25 @@ function App() {
       <section className="list">
         <div className="list__header">
           <div className="list__header-control">
-            <button className="btn btn-transparent">+</button>
+            <button className="btn btn-transparent">-</button>
             <button className="btn btn-transparent">+</button>
           </div>
           <div className="list__search">
-              <input type="text" className="form-control list__search-input" placeholder="Поиск" />
+            <input
+              type="text"
+              className="form-control list__search-input"
+              placeholder="Поиск"
+            />
           </div>
         </div>
         <div className="list__content">
           <div className="list__item list__item--head">
-            <div className="list__item-checkbox"></div>
+            <div className="list__item-checkbox">
+              <label className="checkbox">
+                <input type="checkbox" />
+                <div className="cr"></div>
+              </label>
+            </div>
             <div className="list__item-name">Название</div>
             <div className="list__item-date">Дата</div>
             <div className="list__item-location">Место проведения</div>
@@ -27,7 +36,7 @@ function App() {
           <div className="list__scroll">
             <div className="list__item">
               <div className="list__item-checkbox">
-                <label htmlFor="" className="checkbox">
+                <label className="checkbox">
                   <input type="checkbox" />
                   <div className="cr"></div>
                 </label>
