555e10badc1bbf7861aaa6cf030d5fc8ec44e37c
Ramil <vafarulez@ya.ru>
2019-10-12 11:52:37 +0400

    logger

--- src/store/store.js
+++ src/store/store.js
@@ -6,12 +6,24 @@ import {
 } from "./actions/events/eventsAction";
 import rootReducer from "./reducers/reducers";
 
+/**
+ * Логирует все действия и состояния после того, как действия будут отправлены.
+ */
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info("dispatching", action);
+  let result = next(action);
+  console.log("next state", store.getState());
+  console.groupEnd(action.type);
+  return result;
+};
+
 const fetchEvents = store => next => action => {
   const result = next(action);
 
   if (action.type === FETCH_EVENTS_REQUEST) {
     fetch("/data.json", {
-      method: "GET",
+      method: "GET"
       // mode: "cors"
     })
       .then(response => {
@@ -33,7 +45,7 @@ export default initialStore =>
     initialStore,
 
     compose(
-      applyMiddleware(fetchEvents),
+      applyMiddleware(fetchEvents, logger),
       window.devToolsExtension ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
     )
   );
